19843ca2d4bb3bc8e73c3205fee7f201941add4d
Quentin André <quentin.andre@colorado.edu>
2022-08-19 12:19:05 -0600

    2022-08-19: Updated typo in Figure 1.

--- process_notebook.sh
+++ process_notebook.sh
@@ -9,4 +9,6 @@ else
 jupyter nbconvert --to markdown  "$1/notebook.ipynb"
 fi
 cat "$1/header.md" | grep -Pzo "(?s)(\-{3}(.*?)\-{3})" | sed 's/\x0/\n\n/g' | cat - "$1/notebook.md" | sed "s/notebook_files/files/g" > "$1/index.md"
+rm -r "$1/files"
+mv "$1/notebook_files" "$1/files"
 rm "$1/notebook.md"
